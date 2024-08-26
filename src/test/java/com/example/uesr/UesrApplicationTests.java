67e407a81219210ad372a3c4ba78d814a29dd523
11564 <1156441325@qq.com>
2024-08-26 22:31:15 +0800

    数据

---
+++ src/test/java/com/example/uesr/UesrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uesr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UesrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
